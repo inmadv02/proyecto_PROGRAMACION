e3b69abd6378040bcec06a8f070688db0b191e66
inmadv02 <74898704+inmadv02@users.noreply.github.com>
2021-01-14 13:36:25 +0100

    Add files via upload
    Intentos de mapa

---
+++ ProyectoT03_InmaculadaDominguez_DanielFernandez/src/principal/Ppal.java
@@ -0,0 +1,14 @@
+package principal;
+
+import vistas.Mapa;
+
+public class Ppal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Mapa.imprimirMapa();
+
+	}
+
+}
