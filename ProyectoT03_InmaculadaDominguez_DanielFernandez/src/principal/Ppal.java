22553ee662f315a3884237b0a32368e22a5e1506
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-01-15 18:24:57 +0100

    Título e intro

--- ProyectoT03_InmaculadaDominguez_DanielFernandez/src/principal/Ppal.java
+++ ProyectoT03_InmaculadaDominguez_DanielFernandez/src/principal/Ppal.java
@@ -1,11 +1,15 @@
 package principal;
 
 import vistas.Mapa;
+import vistas.Titulo;
 
 public class Ppal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		Titulo.imprimirTitulo();
 		
 		Mapa.imprimirMapa();
 
--- ProyectoT03_InmaculadaDominguez_DanielFernandez/src/vistas/Historia.java
+++ ProyectoT03_InmaculadaDominguez_DanielFernandez/src/vistas/Historia.java
@@ -1,5 +1,19 @@
 package vistas;
 
 public class Historia {
+	
+	public static void imprimirIntro () {
+		
+		System.out.println("\nBienvenido al año 2020, un año donde la humanidad se ha visto\n"
+				+ " amenazada por un virus nuevo y desconocido: el Coronavirus.");
+		System.out.println("Este patógeno no solo afecta a la salud de las personas, sino que\n "
+				+ "con el paso de los días, la persona contagiada pierde la razón y se vuelve \ntotalmente"
+				+ " agresiva contra cualquier ser viviente.");
+		System.out.println("\nComo consecuencia, los humanos se encuentran en una situación prácticamente"
+				+ " \napocalíptica, en la que viven en constante miedo, ya que su día a día se ha \nconvertido en una"
+				+ "continua lucha por la supervivencia. ");
+		System.out.println("\nCualquier objeto o pertenencia adicional proporciona ventaja sobre el resto\n de la población, así que, "
+				+ " ¿por qué no sales afuera para ver qué más puedes conseguir?");
+	}
 
 }
