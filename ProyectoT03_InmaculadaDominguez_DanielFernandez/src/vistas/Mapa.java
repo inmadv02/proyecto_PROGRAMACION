176389bb463af519d29d66ffb2d27b44faf37ee4
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-01-15 16:21:21 +0100

    Mapa 0.12

--- ProyectoT03_InmaculadaDominguez_DanielFernandez/src/vistas/Mapa.java
+++ ProyectoT03_InmaculadaDominguez_DanielFernandez/src/vistas/Mapa.java
@@ -14,96 +14,96 @@ public class Mapa {
 		
 		for(int i = 0; i < 8; i++) {
 			System.out.print("* ");
-			for(int j = 0; j < 14; j++) {
+			for(int j = 0; j < 11; j++) {
 				System.out.print(" ");
 				
 				if(i == 0 && j == 1) {
-					System.out.print("    ");
+					System.out.print("\t  ");
 				}
 				
 				//Primera calle del supermercado
 				
 				if(i == 1 && j == 1) {
-					System.out.print(" *");
+					System.out.print("||");
 				}
 				
 				if(i == 2 && j == 1) {
-					System.out.print(" *");
+					System.out.print("||");
 				}
 				
 				if(i == 3 && j == 1) {
-				System.out.print(" *");
+				System.out.print("||");
 				
 				}
 				
 				if(i == 4 && j == 1) {
-					System.out.print(" *");
+					System.out.print("||");
 				}
 				
 				if(i == 5 && j == 1) {
-					System.out.print(" *");
+					System.out.print("||");
 				}
 				
 				//Segunda calle del super
 				
-				if(i == 1 && j == 5) {
-					System.out.print("*");
+				if(i == 1 && j == 4) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 2 && j == 5) {
-					System.out.print("*");
+				if(i == 2 && j == 4) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 3 && j == 5) {
-					System.out.print("*");
+				if(i == 3 && j == 4) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 4 && j == 5) {
-					System.out.print("*");
+				if(i == 4 && j == 4) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 5 && j == 5) {
-					System.out.print("*");
+				if(i == 5 && j == 4) {
+					System.out.print("||");
 				
 				}
 				
 				//Tercera calle del super
 				
-				if(i == 1 && j == 9) {
-					System.out.print("*");
+				if(i == 1 && j == 7) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 2 && j == 9) {
-					System.out.print("*");
+				if(i == 2 && j == 7) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 3 && j == 9) {
-					System.out.print("*");
+				if(i == 3 && j == 7) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 4 && j == 9) {
-					System.out.print("*");
+				if(i == 4 && j == 7) {
+					System.out.print("||");
 				
 				}
 				
-				if(i == 5 && j == 9) {
-					System.out.print("*");
+				if(i == 5 && j == 7) {
+					System.out.print("||");
 				
 				}
 				
 				if(i == 6 && j == 10) {
-					System.out.print("    ");
+					System.out.print("\t   ");
 				}
 				
 				if(i == 7 && j == 10) {
-					System.out.print("    ");
+					System.out.print("\t   ");
 				}
 			
 			}
